f8304506b3743c0b36850b18c6becbf37e0013f2
AlvaroMN5 <menanavarroalvaro5@gmail.com>
2025-05-24 18:02:09 +0200

    inicio,imagenes

---
+++ tfg_perfumes/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'includes/init.php';
+require_once 'templates/header.php';
+require_once 'templates/navigation.php';
+include 'pages/catalogo.php';
+require_once 'templates/footer.php';
