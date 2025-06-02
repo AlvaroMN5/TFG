3b61f6275539865c21517386a1a0a385d64cf82f
AlvaroMN5 <menanavarroalvaro5@gmail.com>
2025-06-02 15:54:09 +0200

    arreglo de cabecera y pie de pagin de administrador

---
+++ perfumes/includes/admin_header.php
@@ -0,0 +1,12 @@
+<?php 
+if (session_status() === PHP_SESSION_NONE) session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Panel Admin</title>
+    <link rel="stylesheet" href="../assets/css/admin.css">
+</head>
+<body>
+<div class="admin-container">
