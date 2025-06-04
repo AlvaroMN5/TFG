7781d58419d087dd668df185d0f3bf87d26f0a65
AlvaroMN5 <menanavarroalvaro5@gmail.com>
2025-06-04 19:47:13 +0200

    dandole colores

--- perfumes/includes/admin_header.php
+++ perfumes/includes/admin_header.php
@@ -7,6 +7,10 @@ if (session_status() === PHP_SESSION_NONE) session_start();
     <meta charset="UTF-8">
     <title>Panel Admin</title>
     <link rel="stylesheet" href="../assets/css/admin.css">
+<link rel="stylesheet" href="../assets/css/admin.css">
+<link rel="stylesheet" href="../assets/css/tema_clar.css">
+
+
 </head>
 <body>
 <div class="admin-container">
--- perfumes/includes/header.php
+++ perfumes/includes/header.php
@@ -8,6 +8,9 @@ session_start(); // Siempre al inicio para manejar sesiones
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda de Perfumes</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/tema_clar.css">
+
 </head>
 <body>
     <header>
